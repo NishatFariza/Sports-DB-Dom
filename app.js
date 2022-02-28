790a77b0a0954927c6165aa4effff9f949daee25
NishatFariza <nishatfarzia78@gmail.com>
2022-02-28 18:28:56 +0600

    players error handel done

--- app.js
+++ app.js
@@ -1,17 +1,55 @@
 const loadPlayers = () =>{
     document.getElementById('player-container').innerHTML="";
+    document.getElementById('spinner').style.display="block"
     const searchValue = document.getElementById("search-input").value;
-    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
+
+    if(searchValue == "" || isNaN(searchValue) == false){
+            alert('please enter valid input')
+            document.getElementById('spinner').style.display="none"
+            document.getElementById('player-container').innerHTML="";
+            document.getElementById('search-input').value="";
+            document.getElementById('players-details').innerHTML="";
+        }
+    else{
+        const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
     fetch(url)
     .then( res => res.json())
-    .then(data => searchPlayers(data.player))
+    .then(data => {
+        // console.log(data.player ==null);
+        if(data.player == null){
+            
+            setTimeout(()=>{
+                document.getElementById('spinner').style.display="none"
+                alert('This content is not avaiable')
+                document.getElementById('search-input').value="";
+                //  console.log('hhhhhhhhhhh')
+                document.getElementById('player-container').innerHTML="";
+            }, 5000)
+          
+            
+        }
+        else{
+            searchPlayers(data.player)
+             document.getElementById('spinner').style.display="none"
+             document.getElementById('search-input').value="";
+             
+          
+        }
+    })
+    // searchValue.value="";
+    }
 }
 
 const searchPlayers = players =>{ 
+    if(players ==null){
+        alert('This content is not available')
+    }
     
-    // console.log(players)
+    console.log(players)
     for(const player of players){ 
+        console.log(player)
         const playerContainer =document.getElementById('player-container');
+        // document.getElementById('player-container').innerHTML="";
         const div =document.createElement('div');
         const {strThumb, strPlayer, strNationality, idPlayer}=player
         div.innerHTML =`<div class="card border border-3 py-5 mb-4 rounded-3">
@@ -19,7 +57,7 @@ const searchPlayers = players =>{
          <h2>Name: ${strPlayer}</h2>
          <h3>Country: ${strNationality}</h3>
          <div class="all-buttons pt-3">
-         <button class="py-2 px-4 btn btn-dark">Delete<button>
+         <button class="py-2 px-4 btn btn-dark">Delete</button>
          <button class="py-2 px-4 btn btn-primary" onclick="playerId(${idPlayer})">Player Details</button>
          </div>
         </div>
@@ -39,7 +77,7 @@ const playerId = (id) =>{
 }
 
 const playerDetails = (details) =>{
-    console.log(details)
+    // console.log(details)
     const singlePlayer =document.getElementById('players-details');
     const div1 =document.createElement('div');
     const {strThumb, strPlayer, strNationality}=details
